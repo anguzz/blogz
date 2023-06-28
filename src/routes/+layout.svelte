8b520c428d7ac4cd3f5ea6a2750b65ab1b3be8d2
Angel <santoyox714@gmail.com>
2023-06-27 19:54:39 -0700

    update all teal to blue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,7 +21,7 @@
   <div class="flex flex-col flex-grow w-full px-4 py-2">
     <header class="flex items-center justify-between w-full max-w-2xl py-4 mx-auto lg:pb-8">
       <a
-        class="text-lg font-bold sm:text-2xl !text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-teal-600 dark:to-teal-400"
+        class="text-lg font-bold sm:text-2xl !text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-600 dark:to-blue-400"
         href="/"
       >
         {name}
