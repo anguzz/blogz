1d72edec99a09228e56a250e76b2a5c18a8152ce
Angel <santoyox714@gmail.com>
2023-06-27 20:02:49 -0700

    Update info.js

--- src/lib/info.js
+++ src/lib/info.js
@@ -3,7 +3,7 @@
 // feel free to remove this file.
 
 // URL of your website
-export const website = 'https://www.angs.dev/'
+export const website = 'https://blog.angs.dev/'
 
 export const firstName = 'Angel'
 export const lastName = 'Santoyo'
@@ -12,18 +12,11 @@ export const name = `${firstName} ${lastName}`
 // Used for the landing page & footer of posts
 export const avatar = 'https://www.angs.dev/profile.png'
 export const bio = `\
-I’m ${firstName}, an aspiring software engineer based in California. 
-I'm currently studying computer science at California State University, Fullerton. 
-My interests include web development, and web security. 
-
-In my free time I like to play video games, learn about cars, lift weights, listen to music, and try new foods. 
-
+I’m ${firstName}, an aspiring software engineer based in California studying Computer Science. I write random stuff here!
 `
 
-// Your social usernames, empty strings won't be used.
-// For sake of the live demo, I'm using `username/repo` for github,
-// but you can replace with just your username.
-export const github = 'https://github.com/anguzz'
+
+export const github = 'anguzz'
 export const twitter = ''
 export const linkedin = ''
 export const instagram = ''
